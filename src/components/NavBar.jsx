e7cb8941350cec1934d3055f463f9cbeaf6af727
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-02-15 10:35:58 +0800

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Types from './Types'
 
 const NavBar = (props) => (
     <div className="flex justify-between bg-[#2774ba] h-[60px] px-8 items-center">
-      <h2 className="text-white font-bold text-[1.5rem]">PokeDex</h2>
+      <h2 className="text-white font-bold text-[1.5rem]">Pokemon</h2>
       <div className="flex items-center gap-2">
         <Types
           type={props.type}
